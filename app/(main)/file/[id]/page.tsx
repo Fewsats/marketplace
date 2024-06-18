24dbd9933e77371b2bbc02cb091614035931ac34
Pol Avec <pol.avms@gmail.com>
2024-06-18 15:25:45 +0200

    Update page.tsx

--- app/(main)/file/[id]/page.tsx
+++ app/(main)/file/[id]/page.tsx
@@ -35,7 +35,7 @@ export default async function FilePage({ params }: { params: { id: string } }) {
   const id = params.id;
   const data = id && (await fetchFile(id));
 
-  if (!data || data?.file.status === 'in_review') {
+  if (!data || data?.file.status !== 'valid') {
     notFound();
   }
 
