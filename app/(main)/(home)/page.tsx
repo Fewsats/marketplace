f695c9aba3ae7566cfcd3a515b6a1f0c4cd24287
Victoria <39459423+VictoriaVM@users.noreply.github.com>
2024-06-19 11:43:06 +0300

    opengraph updates

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -4,8 +4,8 @@ import { revalidateTag } from 'next/cache';
 import CatalogComponent from '@/app/(main)/(home)/content';
 
 const metadataBase = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/`
-  : 'http://localhost:3000/';
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : 'http://localhost:3000';
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
   return {
